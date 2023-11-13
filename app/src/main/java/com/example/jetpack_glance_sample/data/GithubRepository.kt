1a5c8457f3b8a29c1ab78aa686909660c8f4fca8
dao0203 <youzaizuoteng872@gmail.com>
2023-11-13 19:28:30 +0900

    implement github repository

--- app/src/main/java/com/example/jetpack_glance_sample/data/GithubRepository.kt
+++ app/src/main/java/com/example/jetpack_glance_sample/data/GithubRepository.kt
@@ -3,5 +3,5 @@ package com.example.jetpack_glance_sample.data
 import com.example.jetpack_glance_sample.model.Contribution
 
 interface GithubRepository {
-    suspend fun getContribution(username: String): Contribution
+    suspend fun getContributions(username: String): List<Contribution>
 }
