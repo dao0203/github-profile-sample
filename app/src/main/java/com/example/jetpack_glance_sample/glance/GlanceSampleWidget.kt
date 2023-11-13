384d86415b7a6b59741da33d1708b7aae7f1a916
dao0203 <youzaizuoteng872@gmail.com>
2023-11-13 11:42:41 +0900

    add app widget ui sample

---
+++ app/src/main/java/com/example/jetpack_glance_sample/glance/GlanceSampleWidget.kt
@@ -0,0 +1,36 @@
+package com.example.jetpack_glance_sample.glance
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.height
+import androidx.glance.text.Text
+
+class GlanceSampleWidget : GlanceAppWidget() {
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+        provideContent {
+            GlanceSampleContent()
+        }
+    }
+
+    @Composable
+    private fun GlanceSampleContent() {
+        Column(
+            modifier = GlanceModifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Hello Glance!",
+                modifier = GlanceModifier.height(100.dp)
+            )
+        }
+    }
+}
