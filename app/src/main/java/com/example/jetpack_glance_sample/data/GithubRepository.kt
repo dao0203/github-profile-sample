c85e578b47faf830a493b4fb4627f9d39694af87
dao0203 <youzaizuoteng872@gmail.com>
2023-11-16 00:19:52 +0900

    add property of time

--- app/src/main/java/com/example/jetpack_glance_sample/data/GithubRepository.kt
+++ app/src/main/java/com/example/jetpack_glance_sample/data/GithubRepository.kt
@@ -3,5 +3,9 @@ package com.example.jetpack_glance_sample.data
 import com.example.jetpack_glance_sample.model.Contribution
 
 interface GithubRepository {
-    suspend fun getContributions(username: String): List<Contribution>
+    suspend fun getContributions(
+        username: String,
+        from: String,
+        to: String
+    ): List<Contribution>
 }
--- app/src/main/java/com/example/jetpack_glance_sample/data/GithubRepositoryImpl.kt
+++ app/src/main/java/com/example/jetpack_glance_sample/data/GithubRepositoryImpl.kt
@@ -1,12 +1,11 @@
 package com.example.jetpack_glance_sample.data
 
 import com.apollographql.apollo3.ApolloClient
-import com.example.GetContributionsQuery
+import com.example.jetpack_glance_sample.GetContributionsQuery
 import com.example.jetpack_glance_sample.model.Contribution
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
@@ -15,16 +14,18 @@ class GithubRepositoryImpl(
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.IO,
     private val apolloClient: ApolloClient
 ) : GithubRepository {
-    override suspend fun getContributions(username: String): List<Contribution> {
+    override suspend fun getContributions(
+        username: String,
+        from: String,
+        to: String
+    ): List<Contribution> {
         return withContext(defaultDispatcher) {
             // 今日の日付と1ヶ月前の日付を取得
-            val today = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
-            val from = today.month.minus(1)
             val response = apolloClient.query(
                 GetContributionsQuery(
                     username = username,
-                    from = from.toString(),
-                    to = today.toString()
+                    from = from,
+                    to = to
                 )
             ).execute()
             val contributions =
