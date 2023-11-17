2dff86c92231722b0a31ccc08ecf8f520f669a3b
dao0203 <youzaizuoteng872@gmail.com>
2023-11-18 04:45:52 +0900

    update intend

--- app/src/main/java/com/example/jetpack_glance_sample/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/jetpack_glance_sample/ui/home/HomeViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.jetpack_glance_sample.domain.GetContributionsForThePastMonthUseCase
 import com.example.jetpack_glance_sample.model.Contribution
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
@@ -13,15 +14,15 @@ class HomeViewModel(
     getContributionsForThePastMonthUseCase: GetContributionsForThePastMonthUseCase
 ) : ViewModel() {
 
-    val uiState = getContributionsForThePastMonthUseCase("dao0203").map {
+    val uiState: StateFlow<HomeUiState> = getContributionsForThePastMonthUseCase("dao0203").map {
         HomeUiState(isLoading = false, contributions = it)
-    }.catch {
-        emit(HomeUiState(isLoading = false, error = it))
-    }.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(5_000),
-        initialValue = HomeUiState()
-    )
+    }
+        .catch { emit(HomeUiState(isLoading = false, error = it)) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = HomeUiState()
+        )
 
 
     data class HomeUiState(
