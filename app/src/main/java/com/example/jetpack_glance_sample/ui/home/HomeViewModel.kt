b97bc0e810ecd50f312acea2c5be93ef7a3b1789
dao0203 <youzaizuoteng872@gmail.com>
2023-11-17 15:46:23 +0900

    add homeviewmodel

---
+++ app/src/main/java/com/example/jetpack_glance_sample/ui/home/HomeViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.jetpack_glance_sample.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jetpack_glance_sample.domain.GetContributionsForThePastMonthUseCase
+import com.example.jetpack_glance_sample.model.Contribution
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(
+    getContributionsForThePastMonthUseCase: GetContributionsForThePastMonthUseCase
+) : ViewModel() {
+
+    val uiState = getContributionsForThePastMonthUseCase("dao0203").map {
+        HomeUiState(isLoading = false, contributions = it)
+    }.catch {
+        emit(HomeUiState(isLoading = false, error = it))
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = HomeUiState()
+    )
+
+
+    data class HomeUiState(
+        val isLoading: Boolean = true,
+        val contributions: List<Contribution> = emptyList(),
+        val error: Throwable? = null
+    )
+}
