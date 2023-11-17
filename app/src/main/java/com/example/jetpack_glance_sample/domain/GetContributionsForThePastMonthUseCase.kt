ecd63f344db6de45a95c1610b3ebda5347701678
dao0203 <youzaizuoteng872@gmail.com>
2023-11-18 04:42:30 +0900

    update usecase

--- app/src/main/java/com/example/jetpack_glance_sample/domain/GetContributionsForThePastMonthUseCase.kt
+++ app/src/main/java/com/example/jetpack_glance_sample/domain/GetContributionsForThePastMonthUseCase.kt
@@ -3,22 +3,31 @@ package com.example.jetpack_glance_sample.domain
 import com.example.jetpack_glance_sample.data.GithubRepository
 import com.example.jetpack_glance_sample.model.Contribution
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.datetime.Clock
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Duration.Companion.days
 
 class GetContributionsForThePastMonthUseCase(
     private val githubRepository: GithubRepository
 ) {
+
+    companion object {
+        private const val TAG = "GetContributionsForThePastMonthUseCase"
+        private const val BETWEEN_DAYS = 31
+    }
+
     operator fun invoke(username: String): Flow<List<Contribution>> = flow {
-        val today = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
-        val from = today.month.minus(1)
+        val today = Clock.System.now()
+        val from = today.minus(BETWEEN_DAYS.days)
 
         githubRepository.getContributions(
             username = username,
             from = from.toString(),
             to = today.toString()
         )
+            .catch { throw it }
+            .collect(::emit)
+
     }
 }
