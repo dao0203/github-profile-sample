2b526eb2198d4d1cec70c30a707113a7ad827c60
dao0203 <youzaizuoteng872@gmail.com>
2023-12-14 00:04:04 +0900

    update sample widget

--- app/src/main/java/com/example/jetpack_glance_sample/widget/GlanceSampleWidget.kt
+++ app/src/main/java/com/example/jetpack_glance_sample/widget/GlanceSampleWidget.kt
@@ -5,32 +5,36 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.appWidgetBackground
 import androidx.glance.appwidget.provideContent
+import androidx.glance.background
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
-import androidx.glance.layout.height
+import androidx.glance.layout.padding
 import androidx.glance.text.Text
 
 class GlanceSampleWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
-        provideContent {
-            GlanceSampleContent()
-        }
+        provideContent { GlanceSampleContent() }
     }
 
     @Composable
     private fun GlanceSampleContent() {
-        Column(
-            modifier = GlanceModifier.fillMaxSize(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "Hello Glance!",
-                modifier = GlanceModifier.height(100.dp)
-            )
+        GlanceTheme {
+            Column(
+                modifier = GlanceModifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .appWidgetBackground()
+                    .background(GlanceTheme.colors.primaryContainer),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Hello Glance!")
+            }
         }
     }
 }
